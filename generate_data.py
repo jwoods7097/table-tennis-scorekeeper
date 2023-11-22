606124e7475a3c1c56b9f5e687e929b355a0dc77
jwoods7097 <jwoods7097@gmail.com>
2023-11-22 15:19:31 -0600

    Fixed ball data

--- generate_data.py
+++ generate_data.py
@@ -9,12 +9,12 @@ for game in games:
     game_type, game_id = game.split('_')
     with open(f'D:\\Downloads\\{game}\\ball_markup.json', 'r') as json_file:
         data = json.load(json_file)
-        frame_nums = [f for f in data.keys() if data[f]['x'] != '-1' and data[f]['y'] != '-1'][:(400 if game_type == 'game' else 250)]
+        frame_nums = [f for f in data.keys() if data[f]['x'] != -1 and data[f]['y'] != -1][:(400 if game_type == 'game' else 250)]
         for file in os.listdir('D:\\Dropbox\\Apps\\cis530\\all-frames'):
             if not (file.endswith('.py') or file.endswith('.ps1')):
                 type, id, frame, ext = re.split('\_|\.', file)
                 if type == game_type and id == game_id and frame in frame_nums:
-                    print("Copying ", file)
+                    print("Copying", file)
                     shutil.copyfile('D:\\Dropbox\\Apps\\cis530\\all-frames\\' + file, "D:\\Dropbox\\Apps\\cis530\\ball-data\\" + ("train" if game_type == 'game' else "valid") + f"\\images\\{type}_{id}_{frame}.jpg")
                     x_center = data[frame]['x'] / 1920
                     y_center = data[frame]['y'] / 1080
