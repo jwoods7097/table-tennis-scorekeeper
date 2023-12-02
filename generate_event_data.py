3f4049ba59212505666a1c2dc80e594311e44e1a
jwoods7097 <jwoods7097@gmail.com>
2023-12-02 03:26:17 -0600

    Added k-fold data for event model

--- generate_event_data.py
+++ generate_event_data.py
@@ -2,9 +2,13 @@ import os
 import json
 import re
 import shutil
+import numpy as np
 
 games = ['game_1', 'game_2', 'game_3', 'game_4', 'game_5', 'test_1', 'test_2']
+file_dict = {"bounce": [], "empty_event": [], "net": []}
 
+# Get labeled frames  
+print("Sorting frames by class...")          
 for game in games:
     game_type, game_id = game.split('_')
     with open(f'D:\\Downloads\\{game}\\events_markup.json', 'r') as json_file:
@@ -14,6 +18,33 @@ for game in games:
             if not (file.endswith('.py') or file.endswith('.ps1')):
                 type, id, frame, ext = re.split('\_|\.', file)
                 if type == game_type and id == game_id and frame in frame_nums:
-                    print("Copying", file)
                     cls = data[frame]
-                    shutil.copyfile('D:\\Dropbox\\Apps\\cis530\\all-frames\\' + file, "D:\\Dropbox\\Apps\\cis530\\event-data\\" + ("train" if game_type == 'game' else "valid") + f"\\{cls}\\{type}_{id}_{frame}.jpg")
+                    file_dict[cls].append(file)
+
+# Create folds from labeled frames
+print("Generating folds...")
+np.random.seed(0)                
+for cls, ls in file_dict.items():
+    data = np.random.choice(ls, (5, 200), False)
+    for i in range(5):
+        valid = data[i]
+        path = f"D:\\Dropbox\\Apps\\cis530\\event-data\\fold{i}\\val\\{cls}"
+        if not os.path.exists(path):
+            os.makedirs(path)
+        for file in valid:
+            shutil.copyfile('D:\\Dropbox\\Apps\\cis530\\all-frames\\' + file, f"{path}\\{file}")
+            print(f"Copying {file} into validation set for fold {i}")
+        
+        train = np.delete(data, i, 0).flatten()
+        path = f"D:\\Dropbox\\Apps\\cis530\\event-data\\fold{i}\\train\\{cls}"
+        if not os.path.exists(path):
+            os.makedirs(path)
+        for file in train:
+            shutil.copyfile('D:\\Dropbox\\Apps\\cis530\\all-frames\\' + file, f"{path}\\{file}")
+            print(f"Copying {file} into train set for fold {i}")
+           
+        # Since yolov8 classify demands a test set to work but doesn't use it
+        path = f"D:\\Dropbox\\Apps\\cis530\\event-data\\fold{i}\\test\\{cls}"
+        if not os.path.exists(path):
+            os.makedirs(path)
+        
